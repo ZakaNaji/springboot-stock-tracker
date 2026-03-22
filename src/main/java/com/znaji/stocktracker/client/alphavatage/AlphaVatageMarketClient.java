16809ba57804543a4d6706d9764fb9cf49f4d189
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-03-22 12:29:11 +0100

    feat: alphavantage client

--- src/main/java/com/znaji/stocktracker/client/alphavatage/AlphaVatageMarketClient.java
+++ src/main/java/com/znaji/stocktracker/client/alphavatage/AlphaVatageMarketClient.java
@@ -1,13 +1,36 @@
 package com.znaji.stocktracker.client.alphavatage;
 
 import com.znaji.stocktracker.client.StockMarketClient;
+import com.znaji.stocktracker.client.alphavatage.dto.AlphaVantageQuoteResponse;
+import com.znaji.stocktracker.client.alphavatage.mapper.AlphaVantageQuoteMapper;
 import com.znaji.stocktracker.model.StockQuote;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class AlphaVatageMarketClient implements StockMarketClient {
+
+    private final AlphaVantageProperties properties;
+    private final RestClient restClient;
+    private final AlphaVantageQuoteMapper mapper;
+
+    public AlphaVatageMarketClient(AlphaVantageProperties properties, RestClient restClient, AlphaVantageQuoteMapper mapper) {
+        this.properties = properties;
+        this.restClient = restClient;
+        this.mapper = mapper;
+    }
+
     @Override
     public StockQuote getStockQuote(String symbol) {
-        return null;
+        AlphaVantageQuoteResponse alphaVantageQuote = restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("function", "GLOBAL_QUOTE")
+                        .queryParam("symbol", symbol)
+                        .queryParam("apikey", properties.getApiKey())
+                        .build())
+                .retrieve()
+                .body(AlphaVantageQuoteResponse.class);
+
+        return mapper.toStockQuote(alphaVantageQuote);
     }
 }
