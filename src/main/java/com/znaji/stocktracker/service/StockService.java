5b8842f1e7a4651371187295de1c73761aac2c60
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-03-23 18:52:17 +0100

    feat (stock overview): service layer

--- src/main/java/com/znaji/stocktracker/service/StockService.java
+++ src/main/java/com/znaji/stocktracker/service/StockService.java
@@ -1,7 +1,9 @@
 package com.znaji.stocktracker.service;
 
+import com.znaji.stocktracker.model.StockOverview;
 import com.znaji.stocktracker.model.StockQuote;
 
 public interface StockService {
     StockQuote getStockQuote(String symbol);
+    StockOverview getStockOverview(String symbol);
 }
--- src/main/java/com/znaji/stocktracker/service/impl/StockServiceImpl.java
+++ src/main/java/com/znaji/stocktracker/service/impl/StockServiceImpl.java
@@ -1,8 +1,10 @@
 package com.znaji.stocktracker.service.impl;
 
 import com.znaji.stocktracker.client.StockMarketClient;
+import com.znaji.stocktracker.model.StockOverview;
 import com.znaji.stocktracker.model.StockQuote;
 import com.znaji.stocktracker.service.StockService;
+import org.jspecify.annotations.NonNull;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,16 @@ public class StockServiceImpl implements StockService {
     @Override
     @Cacheable(value = "stockQuotes", key = "#symbol.trim().toUpperCase()")
     public StockQuote getStockQuote(String symbol) {
-        var normalizedSymbol = symbol.trim().toUpperCase();
+        var normalizedSymbol = getNormalizedSymbol(symbol);
         return stockMarketClient.getStockQuote(normalizedSymbol);
+    }
+
+    @Override
+    public StockOverview getStockOverview(String symbol) {
+        return stockMarketClient.getStockOverview(getNormalizedSymbol(symbol));
+    }
+
+    private String getNormalizedSymbol(String symbol) {
+        return symbol.trim().toUpperCase();
     }
 }
